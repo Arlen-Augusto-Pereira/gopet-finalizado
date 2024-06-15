44b844bd006bfcf88661f17ebd9949c01d494ecf
Arlen-Augusto-Pereira <arlenaugustop@gmail.com>
2024-06-14 23:31:37 -0400

    oi

--- script.js
+++ script.js
@@ -29,3 +29,33 @@ if(pontos2.style.display === "none"){
  botaojava2.innerHTML="Leia Menos"; 
 }
 }
+function leiaMais3(){
+    var pontos3=document.getElementById("pontos3");
+    var maistexto=document.getElementById("mais3");
+    var botaojava3=document.getElementById("botaojava3");
+    
+    if(pontos3.style.display === "none"){
+        pontos3.style.display="inline";
+        maistexto.style.display="none";
+        botaojava3.innerHTML="Leia mais";
+    }else{
+     pontos3.style.display="none";
+     maistexto.style.display="inline";
+     botaojava3.innerHTML="Leia Menos"; 
+    }
+    }
+    function leiaMais4(){
+        var pontos4=document.getElementById("pontos4");
+        var maistexto=document.getElementById("mais4");
+        var botaojava4=document.getElementById("botaojava4");
+        
+        if(pontos4.style.display === "none"){
+            pontos4.style.display="inline";
+            maistexto.style.display="none";
+            botaojava4.innerHTML="Leia mais";
+        }else{
+         pontos4.style.display="none";
+         maistexto.style.display="inline";
+         botaojava4.innerHTML="Leia Menos"; 
+        }
+        }
